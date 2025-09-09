113559a317b7bf3d5ff567eb826f42d7b39a5f5a
Bryan Housel <bhousel@gmail.com>
2025-09-09 16:42:01 -0400

    Move test fixtures into sample file for OsmWikibaseService

---
+++ test/unit/services/OsmWikibaseService.sample.js
@@ -0,0 +1,243 @@
+
+export const keyData = {
+  pageid: 205725,
+  ns: 120,
+  title: 'Item:Q42',
+  lastrevid: 1721242,
+  modified: '2018-12-18T07:00:43Z',
+  type: 'item',
+  id: 'Q42',
+  labels: {
+    fr: {language: 'en', value: 'amenity', 'for-language': 'fr'}
+  },
+  descriptions: {
+    fr: {language: 'en', value: 'English description', 'for-language': 'fr'}
+  },
+  aliases: {},
+  claims: {
+    P2: [ // instance of
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q7'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P16: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'string',
+          datavalue: {value: 'amenity', type: 'string'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P25: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q4679'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P9: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q8'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P6: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q15'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'preferred'
+      },
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q14'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        qualifiers: {
+          P26: [
+            {
+              snaktype: 'value',
+              datatype: 'wikibase-item',
+              datavalue: {value: {'entity-type': 'item', id: 'Q6994'}, type: 'wikibase-entityid'}
+            }
+          ]
+        },
+        rank: 'normal'
+      }
+    ],
+    P28: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'string',
+          datavalue: {value: 'Mapping-Features-Parking-Lot.png', type: 'string'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ]
+  },
+  sitelinks: {
+    wiki: {
+      site: 'wiki',
+      title: 'Key:amenity',
+      badges: []
+    }
+  }
+};
+
+export const tagData = {
+  pageid: 210934,
+  ns: 120,
+  title: 'Item:Q13',
+  lastrevid: 1718041,
+  modified: '2018-12-18T03:51:05Z',
+  type: 'item',
+  id: 'Q13',
+  labels: {
+    fr: {language: 'en', value: 'amenity=parking', 'for-language': 'fr'}
+  },
+  descriptions: {
+    fr: {language: 'fr', value: 'French description'}
+  },
+  aliases: {},
+  claims: {
+    P2: [ // instance of = Q2 (tag)
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q2'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P19: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'string',
+          datavalue: {value: 'amenity=parking', type: 'string'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P10: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q42'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P4: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'commonsMedia',
+          datavalue: {value: 'Primary image.jpg', type: 'string'}
+        },
+        type: 'statement',
+        rank: 'preferred'
+      }
+    ],
+    P6: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q14'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'preferred'
+      },
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q13'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        qualifiers: {
+          P26: [
+            {
+              snaktype: 'value',
+              datatype: 'wikibase-item',
+              datavalue: {value: {'entity-type': 'item', id: 'Q6994'}, type: 'wikibase-entityid'}
+            }
+          ]
+        },
+        rank: 'normal'
+      }
+    ],
+    P25: [
+      {
+        mainsnak: {
+          snaktype: 'value',
+          datatype: 'wikibase-item',
+          datavalue: {value: {'entity-type': 'item', id: 'Q4679'}, type: 'wikibase-entityid'}
+        },
+        type: 'statement',
+        rank: 'normal'
+      }
+    ],
+    P31: [
+      {mainsnak: {datavalue: {value: {text: 'Cs:Key:bridge:movable', language: 'cs'}}}},
+      {mainsnak: {datavalue: {value: {text: 'DE:Key:bridge:movable', language: 'de'}}}},
+      {mainsnak: {datavalue: {value: {text: 'FR:Key:bridge:movable', language: 'fr'}}}},
+      {mainsnak: {datavalue: {value: {text: 'JA:Key:bridge:movable', language: 'ja'}}}},
+      {mainsnak: {datavalue: {value: {text: 'Pl:Key:bridge:movable', language: 'pl'}}}},
+      {mainsnak: {datavalue: {value: {text: 'Key:bridge:movable', language: 'en'}}}},
+    ],
+  },
+  sitelinks: {
+    wiki: {
+      site: 'wiki',
+      title: 'Tag:amenity=parking',
+      badges: []
+    }
+  }
+};
+
+export const localeData = {
+  id: 'Q7792',
+  sitelinks: { wiki: { site: 'wiki', title: 'Locale:fr' } }
+};
+
+
+export const entityResponseSuccess = {
+  entities: {
+    Q42: keyData,
+    Q13: tagData,
+    Q7792: localeData,
+  },
+  success: 1
+};
--- test/unit/services/OsmWikibaseService.test.js
+++ test/unit/services/OsmWikibaseService.test.js
@@ -3,6 +3,7 @@ import { promisify } from 'node:util';
 import { assert } from 'chai';
 import fetchMock from 'fetch-mock';
 import * as Rapid from '../../../modules/headless.js';
+import * as sample from './OsmWikibaseService.sample.js';
 
 
 function parseQueryString(url) {
@@ -98,21 +99,8 @@ describe('OsmWikibaseService', () => {
 
     describe('getEntity', () => {
       it('calls the given callback with the results of the getEntity data item query', () => {
+        fetchMock.route(/action=wbgetentities/, sample.entityResponseSuccess);
         const getEntity = promisify(_wikibase.getEntity).bind(_wikibase);
-
-        fetchMock
-          .route(/action=wbgetentities/, {
-            body: JSON.stringify({
-              entities: {
-                Q42: keyData(),
-                Q13: tagData(),
-                Q7792: localeData,
-              },
-              success: 1
-            }),
-            status: 200,
-            headers: { 'Content-Type': 'application/json' }
-          });
 
         return getEntity({ key: 'amenity', value: 'parking', langCodes: ['fr'] })
           .then(data => {
@@ -128,7 +116,7 @@ describe('OsmWikibaseService', () => {
             };
 
             assert.deepEqual(lastCall, expected);
-            assert.deepEqual(data, { key: keyData(), tag: tagData() });
+            assert.deepEqual(data, { key: sample.keyData, tag: sample.tagData });
           });
       });
     });
@@ -147,14 +135,14 @@ describe('OsmWikibaseService', () => {
     it('gets correct value from entity', () => {
       _wikibase.addLocale('de', 'Q6994');
       _wikibase.addLocale('fr', 'Q7792');
-      assert.strictEqual(_wikibase.claimToValue(tagData(), 'P4', 'en'), 'Primary image.jpg');
-      assert.strictEqual(_wikibase.claimToValue(keyData(), 'P6', 'en'), 'Q15');
-      assert.strictEqual(_wikibase.claimToValue(keyData(), 'P6', 'fr'), 'Q15');
-      assert.strictEqual(_wikibase.claimToValue(keyData(), 'P6', 'de'), 'Q14');
+      assert.strictEqual(_wikibase.claimToValue(sample.tagData, 'P4', 'en'), 'Primary image.jpg');
+      assert.strictEqual(_wikibase.claimToValue(sample.keyData, 'P6', 'en'), 'Q15');
+      assert.strictEqual(_wikibase.claimToValue(sample.keyData, 'P6', 'fr'), 'Q15');
+      assert.strictEqual(_wikibase.claimToValue(sample.keyData, 'P6', 'de'), 'Q14');
     });
 
     it('gets monolingual value from entity as an object', () => {
-      assert.deepEqual(_wikibase.monolingualClaimToValueObj(tagData(), 'P31'), {
+      assert.deepEqual(_wikibase.monolingualClaimToValueObj(sample.tagData, 'P31'), {
         cs: 'Cs:Key:bridge:movable',
         de: 'DE:Key:bridge:movable',
         fr: 'FR:Key:bridge:movable',
@@ -163,246 +151,6 @@ describe('OsmWikibaseService', () => {
         en: 'Key:bridge:movable'
       });
     });
-
   });
-
-
-  function keyData() {
-    return {
-      pageid: 205725,
-      ns: 120,
-      title: 'Item:Q42',
-      lastrevid: 1721242,
-      modified: '2018-12-18T07:00:43Z',
-      type: 'item',
-      id: 'Q42',
-      labels: {
-        fr: {language: 'en', value: 'amenity', 'for-language': 'fr'}
-      },
-      descriptions: {
-        fr: {language: 'en', value: 'English description', 'for-language': 'fr'}
-      },
-      aliases: {},
-      claims: {
-        P2: [ // instance of
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q7'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P16: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'string',
-              datavalue: {value: 'amenity', type: 'string'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P25: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q4679'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P9: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q8'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P6: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q15'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'preferred'
-          },
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q14'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            qualifiers: {
-              P26: [
-                {
-                  snaktype: 'value',
-                  datatype: 'wikibase-item',
-                  datavalue: {value: {'entity-type': 'item', id: 'Q6994'}, type: 'wikibase-entityid'}
-                }
-              ]
-            },
-            rank: 'normal'
-          }
-        ],
-        P28: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'string',
-              datavalue: {value: 'Mapping-Features-Parking-Lot.png', type: 'string'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ]
-      },
-      sitelinks: {
-        wiki: {
-          site: 'wiki',
-          title: 'Key:amenity',
-          badges: []
-        }
-      }
-    };
-  }
-
-  function tagData() {
-    return {
-      pageid: 210934,
-      ns: 120,
-      title: 'Item:Q13',
-      lastrevid: 1718041,
-      modified: '2018-12-18T03:51:05Z',
-      type: 'item',
-      id: 'Q13',
-      labels: {
-        fr: {language: 'en', value: 'amenity=parking', 'for-language': 'fr'}
-      },
-      descriptions: {
-        fr: {language: 'fr', value: 'French description'}
-      },
-      aliases: {},
-      claims: {
-        P2: [ // instance of = Q2 (tag)
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q2'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P19: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'string',
-              datavalue: {value: 'amenity=parking', type: 'string'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P10: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q42'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P4: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'commonsMedia',
-              datavalue: {value: 'Primary image.jpg', type: 'string'}
-            },
-            type: 'statement',
-            rank: 'preferred'
-          }
-        ],
-        P6: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q14'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'preferred'
-          },
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q13'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            qualifiers: {
-              P26: [
-                {
-                  snaktype: 'value',
-                  datatype: 'wikibase-item',
-                  datavalue: {value: {'entity-type': 'item', id: 'Q6994'}, type: 'wikibase-entityid'}
-                }
-              ]
-            },
-            rank: 'normal'
-          }
-        ],
-        P25: [
-          {
-            mainsnak: {
-              snaktype: 'value',
-              datatype: 'wikibase-item',
-              datavalue: {value: {'entity-type': 'item', id: 'Q4679'}, type: 'wikibase-entityid'}
-            },
-            type: 'statement',
-            rank: 'normal'
-          }
-        ],
-        P31: [
-          {mainsnak: {datavalue: {value: {text: 'Cs:Key:bridge:movable', language: 'cs'}}}},
-          {mainsnak: {datavalue: {value: {text: 'DE:Key:bridge:movable', language: 'de'}}}},
-          {mainsnak: {datavalue: {value: {text: 'FR:Key:bridge:movable', language: 'fr'}}}},
-          {mainsnak: {datavalue: {value: {text: 'JA:Key:bridge:movable', language: 'ja'}}}},
-          {mainsnak: {datavalue: {value: {text: 'Pl:Key:bridge:movable', language: 'pl'}}}},
-          {mainsnak: {datavalue: {value: {text: 'Key:bridge:movable', language: 'en'}}}},
-        ],
-      },
-      sitelinks: {
-        wiki: {
-          site: 'wiki',
-          title: 'Tag:amenity=parking',
-          badges: []
-        }
-      }
-    };
-  }
-
-  const localeData = {
-    id: 'Q7792',
-    sitelinks: { wiki: { site: 'wiki', title: 'Locale:fr' } }
-  };
-
 
 });
