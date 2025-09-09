bhousel/Rapid

commit d11846f04ba7dbe519126e67f1434fe74a6c6699

More test work for services
1. Spy on redraws in some of the tests.
Followup from 52d7192c4, spy on redraws instead of `loadedData` events

2. Use xmldom for XML support.
I discovered that the loadedNotes tests were failing because our
OSM notes support still relies on XML, and our fetch_response code was
relying on the browser's built-in DOMParser to parse XML, which
isn't a thing built-in to Node when running tests.

I considered just switching this notes API code over to use the JSON API
(and we should still do that), but I also realized that we use XML for
supporting things like svg, kml, and gpx, and tmcw's togeojson libaray
recommends using `xmldom` instead of the browser's built-in DOMParser.
(see https://github.com/placemark/togeojson?tab=readme-ov-file#protips)
`xmldom` is a small dependency and it makes sense to use it where we need
XML support, so I added it the few places we need a `DOMParser`.

A few things with `xmldom` work a little bit differently,
so I needed to change the `parseNoteXML` code in a few places:
- Need to search for the root `osm` element instead of expecting it in
  `childNodes[0]` (`xmldom` gives us the `<?xml` processinginstruction
  in `childnodes[0]`, and a #text \n newline in `childNodes[1]`)
- `childNodes` is not iterable in v0.8.x of xmldom, but is in v0.9.x
  We can work around this by just `Array.from(childnodes)`.
  It is probably worth staying on v0.8.x for now because that's
  the version wanted by Pixi.js, and it would be dumb to bundle
  multiple versions of `xmldom` if we don't need to.

Again, we really should make sure most of the OSM API code is using
the JSON API and we can probably remove all the XML parsing code.
We still will need XML support anyway, like for parsing 'osc' files
or any of the other ones mentioned above, so `xmldom` will be useful.

modules/util/fetch_response.js
@@ -1,3 +1,4 @@
+import { DOMParser } from '@xmldom/xmldom';
 
 
 /**
@@ -91,6 +92,11 @@ export function utilFetchResponse(response) {
       if (response.status === 204 || response.status === 205) return;  // No Content, Reset Content
       return response.json();
 
+    // bhousel note 9/8/25:  Now prefer xmldom instead of builtin browser DOMParser,
+    // 1. to better handle the togeojson usecases:
+    // 2. and also to support browserless environments (node testing, etc)
+    // see https://github.com/placemark/togeojson?tab=readme-ov-file#protips
+    // see https://github.com/xmldom/xmldom
     // see https://developer.mozilla.org/en-US/docs/Web/API/DOMParser/parseFromString
     case 'application/xhtml+xml':
     case 'application/xml':
@@ -98,7 +104,7 @@ export function utilFetchResponse(response) {
     case 'text/html':
     case 'text/xml':
       return response.text()
-        .then(txt => new window.DOMParser().parseFromString(txt, contentType));
+        .then(txt => new DOMParser().parseFromString(txt, contentType));
 
     case 'application/octet-stream':
     case 'application/protobuf':

modules/headless.js
@@ -107,7 +107,6 @@ export class MockGfxSystem extends MockSystem {
 }
 
 
-
 // Polyfill idle callback functions (for Node)
 if (!globalThis.requestIdleCallback) {
   globalThis.requestIdleCallback = (callback) => {

modules/pixi/PixiLayerCustomData.js
@@ -1,3 +1,4 @@
+import { DOMParser } from '@xmldom/xmldom';
 import { Extent } from '@rapid-sdk/math';
 import { gpx, kml } from '@tmcw/togeojson';
 import { parse as wktParse } from 'wkt';

modules/services/OsmService.js
@@ -932,6 +932,7 @@ export class OsmService extends AbstractSystem {
 
     const context = this.context;
     const cache = this._tileCache;
+    const gfx = context.systems.gfx;
     const spatial = context.systems.spatial;
     const locations = context.systems.locations;
 
@@ -954,6 +955,9 @@ export class OsmService extends AbstractSystem {
         cache.toLoad.delete(tile.id);
         spatial.addTiles('osm-data', [tile]);
       }
+
+      gfx?.deferredRedraw();
+
       if (callback) {
         callback(err, Object.assign({}, results, { tile: tile }));
       }
@@ -1458,11 +1462,11 @@ export class OsmService extends AbstractSystem {
   _parseComments(comments) {
     let parsedComments = [];
 
-    for (const comment of comments) {
+    for (const comment of Array.from(comments)) {
       if (comment.nodeName === 'comment') {
         let parsedComment = {};
 
-        for (const node of comment.childNodes) {
+        for (const node of Array.from(comment.childNodes)) {
           const nodeName = node.nodeName;
           if (nodeName === '#text') continue;
           parsedComment[nodeName] = node.textContent;
@@ -1593,12 +1597,16 @@ export class OsmService extends AbstractSystem {
   _parseXML(xml, callback, options) {
     options = Object.assign({ skipSeen: true }, options);
 
-    if (!xml || !xml.childNodes) {
+    if (!xml?.childNodes) {
       return callback({ message: 'No XML', status: -1 });
     }
 
-    const root = xml.childNodes[0];
-    const children = Array.from(root.childNodes);
+    const osmElement = Array.from(xml.childNodes).find(child => child.nodeName === 'osm');
+    if (!osmElement?.childNodes) {
+      return callback({ message: 'No OSM Element', status: -1 });
+    }
+
+    const children = Array.from(osmElement.childNodes);
     if (children.some(child => child.nodename === 'error')) {
       return callback({ message: 'Partial XML', status: -1 });
     }
@@ -1752,8 +1760,6 @@ export class OsmService extends AbstractSystem {
     const context = this.context;
     const spatial = context.systems.spatial;
 
-    const attrs = xml.attributes;
-    const childNodes = xml.childNodes;
     const props = {
       id: uid,
       serviceID: this.id,
@@ -1761,10 +1767,11 @@ export class OsmService extends AbstractSystem {
     };
 
     // If notes are coincident, move them apart slightly..
+    const attrs = xml.attributes;
     props.loc = spatial.preventCoincidentLoc('osm-notes', this._getLoc(attrs));
 
     // parse note contents
-    for (const node of childNodes) {
+    for (const node of Array.from(xml.childNodes)) {
       const nodeName = node.nodeName;
       if (nodeName === '#text') continue;
 

test/unit/services/EsriService.test.js
@@ -1,4 +1,4 @@
-import { after, before, beforeEach, describe, it } from 'node:test';
+import { after, before, beforeEach, describe, it, mock } from 'node:test';
 import { assert } from 'chai';
 import fetchMock from 'fetch-mock';
 import * as Rapid from '../../../modules/headless.js';
@@ -9,8 +9,15 @@ describe('EsriService', () => {
   // Setup context..
   const context = new Rapid.MockContext();
   context.systems = {
+    gfx:     new Rapid.MockGfxSystem(context),
     spatial: new Rapid.SpatialSystem(context)
   };
+
+  // Spy on redraws..
+  const gfx = context.systems.gfx;
+  const spyRedraw = mock.fn();
+  gfx.immediateRedraw = spyRedraw;
+  gfx.deferredRedraw = spyRedraw;
 
   // Setup fetchMock..
   before(() => {
@@ -22,10 +29,12 @@ describe('EsriService', () => {
 
   after(() => {
     fetchMock.hardReset({ includeSticky: true });
+    mock.reset();
   });
 
   beforeEach(() => {
     fetchMock.removeRoutes().clearHistory();
+    spyRedraw.mock.resetCalls();
   });
 
 

test/unit/services/KartaviewService.sample.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 
 // photos near [10°, 0°]
 export const nearbyPhotos10 = {

test/unit/services/KartaviewService.test.js
@@ -15,7 +15,7 @@ describe('KartaviewService', () => {
     spatial: new Rapid.SpatialSystem(context)
   };
 
-  // Spy on redraws
+  // Spy on redraws..
   const gfx = context.systems.gfx;
   const spyRedraw = mock.fn();
   gfx.immediateRedraw = spyRedraw;

test/unit/services/MapillaryService.test.js
@@ -1,4 +1,4 @@
-import { after, before, beforeEach, describe, it } from 'node:test';
+import { after, before, beforeEach, describe, it, mock } from 'node:test';
 import { assert } from 'chai';
 import fetchMock from 'fetch-mock';
 import * as Rapid from '../../../modules/headless.js';
@@ -8,10 +8,17 @@ describe('MapillaryService', () => {
   // Setup context..
   const context = new Rapid.MockContext();
   context.systems = {
+    gfx:     new Rapid.MockGfxSystem(context),
     l10n:    new Rapid.MockSystem(context),
     photos:  new Rapid.MockSystem(context),
     spatial: new Rapid.SpatialSystem(context)
   };
+
+  // Spy on redraws..
+  const gfx = context.systems.gfx;
+  const spyRedraw = mock.fn();
+  gfx.immediateRedraw = spyRedraw;
+  gfx.deferredRedraw = spyRedraw;
 
   // Setup fetchMock..
   before(() => {
@@ -20,10 +27,12 @@ describe('MapillaryService', () => {
 
   after(() => {
     fetchMock.hardReset({ includeSticky: true });
+    mock.reset();
   });
 
   beforeEach(() => {
     fetchMock.removeRoutes().clearHistory();
+    spyRedraw.mock.resetCalls();
   });
 
 

commit a35d20763d93d26ca4a78bbe478203f43af0f069

Allow fetch_response to use the browser's DOMParser if needed
More fallout from d11846f04
We will still need the browser's DOMParser in some situations.
For example, to insert an svg spritesheet into the document.

modules/ui/UiDefs.js
@@ -77,7 +77,8 @@ export class UiDefs {
         const $group = select(nodes[i]);
         const url = assets.getFileURL(`img/${d}-sprite.svg`);
         fetch(url)
-          .then(utilFetchResponse)
+          // We need the browser's DOMParser here, so we can insert this spritesheet into the document.
+          .then(response => utilFetchResponse(response, new window.DOMParser()))
           .then(svg => $group.call(this._spritesheetLoaded, d, svg))
           .catch(e => console.error(e));  // eslint-disable-line
       });

modules/util/fetch_response.js
@@ -32,11 +32,12 @@ export class FetchError extends Error {
  *      if (err.name === 'FetchError') …        // deal with error
  *   })
  *
- * @param    {Response}   The `Response` from a `fetch`
+ * @param    {Response}   response  - The `Response` from a `fetch`
+ * @param    {DOMParser}  domParser - Optional, specify a DOMParser to handle XML with
  * @returns  {*}          Result suitable to be returned to a `.then()` (a value or Promise)
  * @throws   {FetchError}
  */
-export function utilFetchResponse(response) {
+export function utilFetchResponse(response, domParser) {
   if (!response.ok) {
     throw new FetchError(response);
   }
@@ -104,7 +105,10 @@ export function utilFetchResponse(response) {
     case 'text/html':
     case 'text/xml':
       return response.text()
-        .then(txt => new DOMParser().parseFromString(txt, contentType));
+        .then(txt => {
+          if (!domParser) domParser = new DOMParser();  // use xmldom parser unless specified
+          return domParser.parseFromString(txt, contentType)
+        });
 
     case 'application/octet-stream':
     case 'application/protobuf':