bhousel/Rapid

commit 7096e1e731cbc1a3a08709fe43f512061ba025e7

Attempting to consolidate all simple one-word texts under 'text.'
A lot of these appeared in duplicate in various parts of the app
and it makes it easier to just have a `text.` namespace to keep them
instead of there being a bunch of different "Okay"s etc.

modules/ui/UiRapidInspector.ts
@@ -396,7 +396,7 @@ export class UiRapidInspector {
     $tagInfo = $tagInfo.merge($$tagInfo);
 
     $tagInfo.selectAll('.tag-heading')
-      .text(l10n.t('rapid_inspector.tags'));
+      .text(l10n.t('rapid_text.tags'));
   }
 
 

commit d1ac112e59781f9dcb6fa055c31b028fd542a6d0

Fix stringID copypaste regression from 7096e1e73

modules/ui/UiRapidInspector.ts
@@ -396,7 +396,7 @@ export class UiRapidInspector {
     $tagInfo = $tagInfo.merge($$tagInfo);
 
     $tagInfo.selectAll('.tag-heading')
-      .text(l10n.t('rapid_text.tags'));
+      .text(l10n.t('text.tags'));
   }
 
 